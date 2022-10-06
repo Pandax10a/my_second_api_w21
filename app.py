01cce8a3acf39e13d3b35cf52ca5e63657f01ace
panda1 <blueblue0011@gmail.com>
2022-10-06 15:05:30 -0700

    trying to make post work

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# modules to make this work, flask for api end point, dbhelpers for connection, json for data in json
 from flask import Flask, request
 import dbhelpers as dh
 import json
@@ -5,12 +6,26 @@ import json
 app = Flask(__name__)
 
 @app.get('/api/inventory')
-
+# return id, name, description, stock, created_at
 def inventory_list():
     result = dh.run_statement('CALL inventory_list()')
     if(type(result) == list):
         inventory_json = json.dumps(result, default=str)
         return inventory_json
+    else:
+        print('error, error, error ')
+
+# needs 3 argument(name, description, quantity) to add new item to db and returns the item's id as json
+@app.post('/api/add_new_item')
+
+def add_new_item():
+    result = dh.run_statement('CALL add_new_item()')
+    name_input = request.args.get('name')
+    description_input = request.args.get('description')
+    stock_input = request.args.get('stock')
+    if(type(result) == list):
+        client_json = json.dumps(result, default=str)
+        return client_json
     else:
         print('error, error, error ')
 
