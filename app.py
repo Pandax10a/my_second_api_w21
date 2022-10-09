8d0a87ad91826fc84d2f603e8d7d9523e57d5f86
panda1 <blueblue0011@gmail.com>
2022-10-09 16:06:27 -0700

    employee get works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # modules to make this work, flask for api end point, dbhelpers for connection, json for data in json
+from unittest import result
 from flask import Flask, request, make_response
 import dbhelpers as dh
 import json
@@ -6,7 +7,7 @@ import apihelper as a
 
 app = Flask(__name__)
 
-@app.get('/api/inventory')
+@app.get('/api/item')
 # return id, name, description, stock, created_at
 def inventory_list():
     result = dh.run_statement('CALL inventory_list()')
@@ -19,12 +20,17 @@ def inventory_list():
         # print('error, error, error ')
 
 # needs 3 argument(name, description, quantity) to add new item to db and returns the item's id as json
-@app.post('/api/add_new_item')
+@app.post('/api/item')
 # inserts new item into db, only requires 3 input name, description, stock
 def add_new_item():
     name_input = request.json.get('name')
     description_input = request.json.get('description')
     stock_input = request.json.get('stock')
+
+    valid_check = a.check_endpoint_info(request.json, ['name', 'description', 'stock'])
+    if(type(valid_check) == str):
+        return valid_check
+
     result = dh.run_statement('CALL add_new_item(?,?,?)', [name_input, description_input, stock_input])
     if(type(result) == list):
         # using make response to make it easier for me to debug
@@ -36,14 +42,50 @@ def add_new_item():
         # print('error, error, error ')
 
 #add and update current item in database and returns the value at the end
-@app.patch('/api/update_inventory_item')
+@app.patch('/api/item')
 def add_and_update_item():
     id_input = request.json.get('id')
     quantity_added = request.json.get('add_qt')
+    # check to see if the value enter is valid(string)
+    valid_check = a.check_endpoint_info(request.json, ['id', 'add_qt'])
+    if(type(valid_check) == str):
+        return valid_check
+
     result = dh.run_statement('CALL add_and_update_item(?,?)', [id_input, quantity_added])
     if(type(result) == list):
+        # http response 200 for success, 400 for fail
         return make_response(json.dumps(result, default=str), 200)
 
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+#give id and delete item from db
+@app.delete('/api/item')
+def remove_item():
+    item_id = request.json.get('id')
+    valid_check = a.check_endpoint_info(request.json, ['id'])
+    if(type(valid_check) == str):
+        return valid_check
+    result = dh.run_statement('CALL delete_inventory_item(?)', [item_id])
+    if(type(result) == list):
+
+        return make_response(json.dumps(result, default=str), 200)
+
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+
+@app.get('/api/employee')
+# input an id, to return employee name, position, hired_at, hourly wage from db
+def search_employee_by_id():
+    emp_id = request.args.get('id')
+    valid_check = a.check_endpoint_info(request.args, ['id'])
+    if(type(valid_check) == str):
+        return valid_check 
+    result = dh.run_statement('CALL employee_search(?)', [emp_id])
+    if(type(result) == list):
+        return make_response(json.dumps(result, default=str), 200)
+       
     else:
         return make_response(json.dumps(result, default=str), 400)
 
@@ -51,3 +93,4 @@ def add_and_update_item():
 
 
 app.run(debug=True)
+
